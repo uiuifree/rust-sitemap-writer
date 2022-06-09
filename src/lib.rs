a03ffa098b91bd5ff171122e093f3fa386b1d3aa
uiuifree <info@uiuifree.com>
2022-06-09 22:22:17 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -10,43 +10,6 @@ pub enum SitemapError {
 pub struct SitemapWriter {}
 
 impl SitemapWriter {
-    /// # Examples
-    /// ```rust
-    ///  use sitemap::{SitemapWriter,SitemapUrl,SitemapChangeFreq};
-    ///  let write = SitemapWriter::make("test.xml", vec![
-    ///      SitemapUrl{
-    ///         loc: "https://example.com/".to_string(),
-    ///         lastmod: Some("2021-01-01".to_string()),
-    ///         changefreq: Some(SitemapChangeFreq::ALWAYS),
-    ///         priority: Some(1.0),
-    ///     },
-    ///     SitemapUrl::new("https://example.com/contact/"),
-    ///     SitemapUrl::new("https://example.com/contact/?test=1"),
-    ///     SitemapUrl::new("https://example.com/contact/?test=<>"),
-    ///   ]);
-    ///  assert!(write.is_ok())
-    /// ```
-    /// # Output XML
-    /// ```xml
-    ///  <?xml version="1.0" encoding="UTF-8"?>
-    ///  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ///      <url>
-    ///          <loc>https://example.com/</loc>
-    ///          <lastmod>2021-01-01</lastmod>
-    ///          <changefreq>always</changefreq>
-    ///          <priority>1</priority>
-    ///      </url>
-    ///      <url>
-    ///          <loc>https://example.com/contact/</loc>
-    ///      </url>
-    ///      <url>
-    ///          <loc>https://example.com/contact/?test=1</loc>
-    ///      </url>
-    ///      <url>
-    ///          <loc>https://example.com/contact/?test=&lt;&gt;</loc>
-    ///      </url>
-    ///  </urlset>
-    /// ```
     pub fn make(path: &str, urls: Vec<SitemapUrl>)
                 -> Result<(), SitemapError>
     {
@@ -130,7 +93,7 @@ pub enum SitemapChangeFreq {
 
 impl Debug for SitemapChangeFreq {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let _ = f.write_str(self.to_string().as_str());
+        f.write_str(self.to_string().as_str())
     }
 }
 
