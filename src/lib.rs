16d51471905a39dd9df90da48b2034a50fabedf6
uiuifree <info@uiuifree.com>
2023-11-01 08:11:08 +0900

    書き出し

--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,17 @@ pub enum SitemapError {
 pub struct SitemapWriter {}
 
 impl SitemapWriter {
-    pub fn make(path: &str, urls: Vec<SitemapUrl>)
-                -> Result<(), SitemapError>
-    {
+    pub fn make(path: &str, urls: Vec<SitemapUrl>) -> Result<(), SitemapError> {
         let file = File::create(path);
         if file.is_err() {
             return Err(SitemapError::FileOpen(file.err().unwrap().to_string()));
         }
         let mut file = file.unwrap();
         write_text(&file, r#"<?xml version="1.0" encoding="UTF-8"?>"#)?;
-        write_text(&file, r#"<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"#)?;
+        write_text(
+            &file,
+            r#"<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"#,
+        )?;
 
         for url in urls {
             let mut row = "<url>".to_string();
@@ -28,7 +29,11 @@ impl SitemapWriter {
                 row += format!("<lastmod>{}</lastmod>", url.lastmod.unwrap_or_default()).as_str();
             }
             if url.changefreq.is_some() {
-                row += format!("<changefreq>{}</changefreq>", url.changefreq.unwrap().to_string()).as_str();
+                row += format!(
+                    "<changefreq>{}</changefreq>",
+                    url.changefreq.unwrap().to_string()
+                )
+                .as_str();
             }
             if url.priority.is_some() {
                 row += format!("<priority>{}</priority>", url.priority.unwrap()).as_str();
@@ -38,14 +43,12 @@ impl SitemapWriter {
             write_text(&file, row.as_str())?;
         }
         write_text(&file, r#"</urlset> "#)?;
-        match file.flush(){
+        match file.flush() {
             Ok(_) => Ok(()),
-            Err(e) =>SitemapError::Write(e.to_string())
+            Err(e) => Err(SitemapError::Write(e.to_string())),
         }
-
     }
 }
-
 
 fn write_text(mut file: &File, str: &str) -> Result<(), SitemapError> {
     let f = file.write(str.as_bytes());
@@ -73,7 +76,6 @@ impl Default for SitemapUrl {
         }
     }
 }
-
 
 impl SitemapUrl {
     pub fn new(loc: &str) -> SitemapUrl {
@@ -104,13 +106,13 @@ impl Debug for SitemapChangeFreq {
 impl SitemapChangeFreq {
     fn to_string(&self) -> String {
         match self {
-            SitemapChangeFreq::ALWAYS => { "always".to_string() }
-            SitemapChangeFreq::HOURLY => { "hourly".to_string() }
-            SitemapChangeFreq::DAILY => { "daily".to_string() }
-            SitemapChangeFreq::WEEKLY => { "weekly".to_string() }
-            SitemapChangeFreq::MONTHLY => { "monthly".to_string() }
-            SitemapChangeFreq::YEARLY => { "yearly".to_string() }
-            SitemapChangeFreq::NEVER => { "never".to_string() }
+            SitemapChangeFreq::ALWAYS => "always".to_string(),
+            SitemapChangeFreq::HOURLY => "hourly".to_string(),
+            SitemapChangeFreq::DAILY => "daily".to_string(),
+            SitemapChangeFreq::WEEKLY => "weekly".to_string(),
+            SitemapChangeFreq::MONTHLY => "monthly".to_string(),
+            SitemapChangeFreq::YEARLY => "yearly".to_string(),
+            SitemapChangeFreq::NEVER => "never".to_string(),
         }
     }
 }
@@ -124,17 +126,20 @@ mod tests {
         let res = SitemapWriter::make("test.xml", vec![]);
         assert!(res.is_ok());
 
-        let res = SitemapWriter::make("test.xml", vec![
-            SitemapUrl {
-                loc: "https://example.com/".to_string(),
-                lastmod: Some("2021-01-01".to_string()),
-                changefreq: Some(SitemapChangeFreq::ALWAYS),
-                priority: Some(1.0),
-            },
-            SitemapUrl::new("https://example.com/contact/"),
-            SitemapUrl::new("https://example.com/contact/?test=1"),
-            SitemapUrl::new("https://example.com/contact/?test=<>"),
-        ]);
+        let res = SitemapWriter::make(
+            "test.xml",
+            vec![
+                SitemapUrl {
+                    loc: "https://example.com/".to_string(),
+                    lastmod: Some("2021-01-01".to_string()),
+                    changefreq: Some(SitemapChangeFreq::ALWAYS),
+                    priority: Some(1.0),
+                },
+                SitemapUrl::new("https://example.com/contact/"),
+                SitemapUrl::new("https://example.com/contact/?test=1"),
+                SitemapUrl::new("https://example.com/contact/?test=<>"),
+            ],
+        );
         assert!(res.is_ok());
     }
 }
